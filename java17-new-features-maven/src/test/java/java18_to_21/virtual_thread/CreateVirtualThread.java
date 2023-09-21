ceeae8a5b9012b77d0e68902cce4988283a7e2f1
Mahdi Tilab <mahdi.tilab@solvians.com>
2023-09-21 17:42:29 +0200

    Add ExecutorService and CompletableFuture in VirtualThread History

--- java17-new-features-maven/src/test/java/java18_to_21/virtual_thread/CreateVirtualThread.java
+++ java17-new-features-maven/src/test/java/java18_to_21/virtual_thread/CreateVirtualThread.java
@@ -1,10 +1,17 @@
 package java18_to_21.virtual_thread;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.Callable;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 
 public class CreateVirtualThread {
 
-    public static void main(String[] args) throws InterruptedException {
+    public static void main(String[] args) throws InterruptedException, ExecutionException {
 
         // Traditional 1: java 1.0 - Thread class
         Thread thread1 = new Thread() {
@@ -27,9 +34,39 @@ public class CreateVirtualThread {
 
         // Traditional 3: java 1.8 - Runnable interface with lambda and functional interface
         Runnable runnable3 =
-                () -> { System.out.println("Lambda Runnable running"); };
+                () -> System.out.println("Lambda Runnable running");
 
         runnable3.run();
+
+        // ExecuteService: java 1.8 - Execute multiple threads in a single task queue
+        try (ExecutorService singleTaskQueueExecutor = Executors.newFixedThreadPool(3)) {
+            List<String> finalResultList = new ArrayList<>();
+            for (int i = 0; i < 10; i++) {
+                Future<Integer> futureResult = singleTaskQueueExecutor.submit(new Add(10, 20)); // 2
+                Integer intermediateResult = futureResult.get(); // blocking // 3
+                Future<Integer> finalResult = singleTaskQueueExecutor.submit(new Multiply(intermediateResult)); // 4
+                finalResultList.add(i + " " + finalResult.get()); // blocking //5
+            }
+            singleTaskQueueExecutor.shutdown();
+        }
+
+        // CompletableFuture: java 1.8 - Execute multiple threads in a multiple task queue
+        ExecutorService executor = Executors.newFixedThreadPool(3);
+        List<CompletableFuture<Integer>> finalResultList = new ArrayList<>();
+
+        for (int i = 0; i < 10; i++)  {
+            CompletableFuture<Integer> voidCompletableFuture = CompletableFuture
+                    .supplyAsync(() -> add(10, 20), executor)
+                    .thenApplyAsync(CreateVirtualThread::multiply, executor);
+
+            finalResultList.add(voidCompletableFuture);
+        }
+
+        for (CompletableFuture<Integer> future : finalResultList) {
+            future.get();
+        }
+        executor.shutdown();
+
 
         // Platform Thread
         var platformThread = Thread.ofPlatform().unstarted(() -> System.out.println(Thread.currentThread()));
@@ -52,10 +89,10 @@ public class CreateVirtualThread {
         Thread.startVirtualThread(runnable);
 
         // 3
-        try (var executor = Executors.newVirtualThreadPerTaskExecutor()) {
+        try (var virtualThreadPerTaskExecutor = Executors.newVirtualThreadPerTaskExecutor()) {
 
-            executor.submit(runnable);
-            executor.submit(runnable);
+            virtualThreadPerTaskExecutor.submit(runnable);
+            virtualThreadPerTaskExecutor.submit(runnable);
         }
 
 
@@ -73,5 +110,44 @@ public class CreateVirtualThread {
 
         6. 100% compatible with Platform Threads
         */
+    }
+
+    static class Add implements Callable<Integer> {
+        int a;
+        int b;
+
+        Add(int a, int b) {
+            this.a = a;
+            this.b = b;
+        }
+
+        @Override
+        public Integer call() throws Exception {
+            return a + b;
+        }
+    }
+
+    static class Multiply implements Callable<Integer> {
+        int result;
+        Multiply(int result) {
+            this.result = result;
+        }
+
+        @Override
+        public Integer call() throws Exception {
+            result = result * 10;
+            System.out.println(Thread.currentThread().getName() + " " + result);
+            return result;
+        }
+    }
+
+    public static Integer add(int a, int b) {
+        return a + b;
+    }
+
+    public static Integer multiply(int a) {
+        a = a * 10;
+        System.out.println(Thread.currentThread().getName() + " " + a);
+        return a;
     }
 }
